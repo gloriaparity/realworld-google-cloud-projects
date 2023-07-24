ffbd506d0a314018f4aba8f2c893fc32d13b73fa
Gloria Milele <138536313+gloriaparity@users.noreply.github.com>
2023-07-23 21:15:52 -0700

    Update app-automation.sh

--- startup-script/app-automation.sh
+++ startup-script/app-automation.sh
@@ -33,7 +33,7 @@ sudo rm -f latest.tar.gz
 
 # Configure Apache
 sudo rm -rf /etc/apache2/sites-enabled/000-default.conf
-sudo wget https://raw.githubusercontent.com/gloriaparity/realworld-google-cloud-projects/main/database-config/wp-config.php -P /etc/apache2/sites-enabled/
+sudo wget https://raw.githubusercontent.com/awanmbandi/google-cloud-projects/main/apache-config/000-default.conf -P /etc/apache2/sites-enabled/
 
 # Restart the Apache2 Service
 sudo service apache2 restart
