a4876ca191d32043dffdd8ae8532ba5e1bd43ad9
Gloria Milele <138536313+gloriaparity@users.noreply.github.com>
2023-07-24 18:08:13 -0700

    Update wp-config.php

--- database-config/wp-config.php
+++ database-config/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'admin' );
 
 /** Database hostname */
-define( 'DB_HOST', '104.197.80.224' );
+define( 'DB_HOST', '34.69.244.206' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
